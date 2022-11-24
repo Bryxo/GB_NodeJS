75617af2ea1fbde68dbcddf073fa7bcfde2d8c6c
Bryxo <bryxo.job@yandex.ru>
2022-11-24 17:14:46 +0300

    update homework2

--- timerProg.js
+++ timerProg.js
@@ -1,31 +1,43 @@
-let timer = setInterval(function updateTimer() {
-   let future = Date.parse("october 30, 2022 21:49:00");
-   let now = new Date();
-   let diff = future - now;
+import EventEmitter from 'events'
+const emitter = new EventEmitter()
+import moment from "moment";
+import 'moment-precise-range-plugin'
 
-   let years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-   let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-   let hours = Math.floor(diff / (1000 * 60 * 60));
-   let mins = Math.floor(diff / (1000 * 60));
-   let secs = Math.floor(diff / 1000);
+const [userPastDate] = process.argv.slice(2) 
+const format = 'YYYY-MM-DD HH'
 
-   let y = years;
-   let d = days - years * 365;
-   let h = hours - days * 24;
-   let m = mins - hours * 60;
-   let s = secs - mins * 60;
+const getDateFromDateString = dateString => {
+	const [hour, day, month, year] = dateString.split('-')
+	return new Date(Date.UTC(year, month - 1, day, hour))
+}
 
-   console.log(`Осталось ${y} лет; ${d} дней; ${h} часов; ${m} минут; ${s} секунд`)
-   
-   if (y===0 && d===0 && h===0 && m===0 && s===0){
-      clearInterval(timer);
-      console.log("Стоп таймер");
-   };
+const dateInFuture = getDateFromDateString(userPastDate)
 
-   if (y<0){
-      clearInterval(timer);
-      console.log("Мы не можем прыгать в прошлое)");
-      return;
-   };
-  ;
-}, 1000);
+const showRemainingTime = dateInFuture => {
+	const dateNow = new Date()
+
+	if (dateNow >= dateInFuture) {
+		emitter.emit('timerEnd')
+	} else {
+		const currentDateFormatted = moment(dateNow, format)
+		const futureDateFormatted = moment(dateInFuture, format)
+		const diff = moment.preciseDiff(currentDateFormatted, futureDateFormatted)
+      //console.log(futureDateFormatted)
+		console.log(diff)
+	}
+}
+
+const timerId = setInterval(() => {
+	emitter.emit('timerTick', dateInFuture)
+}, 1000)
+
+const showTimerDone = timerId => {
+	clearInterval(timerId)
+	console.log('End')
+}
+
+emitter.on('timerTick', showRemainingTime)
+
+emitter.on('timerEnd', () => {
+	showTimerDone(timerId)
+})
